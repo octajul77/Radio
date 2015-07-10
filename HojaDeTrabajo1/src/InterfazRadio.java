6156cb7a520d7ef33ecb3a5df04c8b99b5c4b7b4
Diego <Diego@DiegoSosa>
2015-07-10 11:30:38 -0600

    Interfaz

---
+++ HojaDeTrabajo1/src/InterfazRadio.java
@@ -0,0 +1,43 @@
+public interface InterfazRadio {
+	final int CONST_CAMBIO_AM = 10;
+	final double CONST_CAMBIO_FM = 0.2; 
+	final int MAX_AM = 1610;
+	final int MIN_AM = 530;
+	final int MAX_FM = 107.9;
+	final int MIN_FM = 87.9;
+	
+	
+	public void EncenderApagar(){
+		
+	}
+	
+	public void CambiarFrec(){
+		
+	}
+	
+	public void CargarEmisora(double emisora){
+		
+	}
+	
+	public void AdelantarEmisora(){
+		
+	}
+	
+	public void AtrasarEmisora(){
+			
+	}
+	
+	public void GuardarEmisora(double emisora, int posicion){
+		
+		
+	}
+	
+	public void SubirVolumen(){
+		
+	}
+	
+	public void BajarVolumen(){
+		
+	}
+	
+}
