7eed02bcbf1f3ac3eff379bacca3c6629d3126cf
Diego <Diego@DiegoSosa>
2015-07-10 11:58:11 -0600

    Reorganice nombre de los botones para aplicar los eventos

--- HojaDeTrabajo1/src/GUII.java
+++ HojaDeTrabajo1/src/GUII.java
@@ -41,77 +41,77 @@ public class GUII extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("ON/OFF");
-		btnNewButton.setBounds(24, 11, 89, 23);
-		contentPane.add(btnNewButton);
+		JButton btnONOFF = new JButton("ON/OFF");
+		btnONOFF.setBounds(24, 11, 89, 23);
+		contentPane.add(btnONOFF);
 		
 		JButton btnAmfm = new JButton("AM/FM");
 		btnAmfm.setBounds(514, 11, 89, 23);
 		contentPane.add(btnAmfm);
 		
-		JButton button = new JButton("--->");
-		button.setBounds(514, 132, 89, 23);
-		contentPane.add(button);
+		JButton buttonLtoR = new JButton("--->");
+		buttonLtoR.setBounds(514, 132, 89, 23);
+		contentPane.add(buttonLtoR);
 		
-		JButton button_1 = new JButton("<---");
-		button_1.setBounds(10, 132, 89, 23);
-		contentPane.add(button_1);
+		JButton buttonRtoL = new JButton("<---");
+		buttonRtoL.setBounds(10, 132, 89, 23);
+		contentPane.add(buttonRtoL);
 		
-		JButton btnNewButton_1 = new JButton("1");
-		btnNewButton_1.setBounds(10, 179, 39, 23);
-		contentPane.add(btnNewButton_1);
+		JButton btn1 = new JButton("1");
+		btn1.setBounds(10, 179, 39, 23);
+		contentPane.add(btn1);
 		
-		JButton button_2 = new JButton("2");
-		button_2.setBounds(59, 179, 40, 23);
-		contentPane.add(button_2);
+		JButton btn2 = new JButton("2");
+		btn2.setBounds(59, 179, 40, 23);
+		contentPane.add(btn2);
 		
-		JButton button_3 = new JButton("3");
-		button_3.setBounds(109, 179, 40, 23);
-		contentPane.add(button_3);
+		JButton btn3 = new JButton("3");
+		btn3.setBounds(109, 179, 40, 23);
+		contentPane.add(btn3);
 		
-		JButton button_4 = new JButton("4");
-		button_4.setBounds(159, 179, 40, 23);
-		contentPane.add(button_4);
+		JButton btn4 = new JButton("4");
+		btn4.setBounds(159, 179, 40, 23);
+		contentPane.add(btn4);
 		
-		JButton button_5 = new JButton("5");
-		button_5.setBounds(209, 179, 40, 23);
-		contentPane.add(button_5);
+		JButton btn5 = new JButton("5");
+		btn5.setBounds(209, 179, 40, 23);
+		contentPane.add(btn5);
 		
-		JButton button_6 = new JButton("6");
-		button_6.setBounds(259, 179, 40, 23);
-		contentPane.add(button_6);
+		JButton btn6 = new JButton("6");
+		btn6.setBounds(259, 179, 40, 23);
+		contentPane.add(btn6);
 		
-		JButton button_7 = new JButton("7");
-		button_7.setBounds(309, 179, 40, 23);
-		contentPane.add(button_7);
+		JButton btn7 = new JButton("7");
+		btn7.setBounds(309, 179, 40, 23);
+		contentPane.add(btn7);
 		
-		JButton button_8 = new JButton("8");
-		button_8.setBounds(359, 179, 40, 23);
-		contentPane.add(button_8);
+		JButton btn8 = new JButton("8");
+		btn8.setBounds(359, 179, 40, 23);
+		contentPane.add(btn8);
 		
-		JButton button_9 = new JButton("9");
-		button_9.setBounds(409, 179, 40, 23);
-		contentPane.add(button_9);
+		JButton btn9 = new JButton("9");
+		btn9.setBounds(409, 179, 40, 23);
+		contentPane.add(btn9);
 		
-		JButton button_10 = new JButton("10");
-		button_10.setBounds(459, 179, 45, 23);
-		contentPane.add(button_10);
+		JButton btn10 = new JButton("10");
+		btn10.setBounds(459, 179, 45, 23);
+		contentPane.add(btn10);
 		
-		JButton button_11 = new JButton("11");
-		button_11.setBounds(514, 179, 45, 23);
-		contentPane.add(button_11);
+		JButton btn11 = new JButton("11");
+		btn11.setBounds(514, 179, 45, 23);
+		contentPane.add(btn11);
 		
-		JButton button_12 = new JButton("12");
-		button_12.setBounds(569, 179, 55, 23);
-		contentPane.add(button_12);
+		JButton btn12 = new JButton("12");
+		btn12.setBounds(569, 179, 55, 23);
+		contentPane.add(btn12);
 		
-		JButton button_13 = new JButton("+");
-		button_13.setBounds(24, 51, 45, 23);
-		contentPane.add(button_13);
+		JButton btnPlus = new JButton("+");
+		btnPlus.setBounds(24, 51, 45, 23);
+		contentPane.add(btnPlus);
 		
-		JButton btnNewButton_2 = new JButton("-");
-		btnNewButton_2.setBounds(24, 85, 45, 23);
-		contentPane.add(btnNewButton_2);
+		JButton btnMinus = new JButton("-");
+		btnMinus.setBounds(24, 85, 45, 23);
+		contentPane.add(btnMinus);
 		
 		txtRadio = new JTextField();
 		txtRadio.setText("Radio 1.0");
