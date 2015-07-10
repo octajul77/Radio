30c6b0fffa670f6de8a7143c80c1bf6863fcd2f0
Diego <Diego@DiegoSosa>
2015-07-10 11:38:07 -0600

    Metodos abastractos no necesitan cuerpo

--- HojaDeTrabajo1/src/InterfazRadio.java
+++ HojaDeTrabajo1/src/InterfazRadio.java
@@ -3,41 +3,24 @@ public interface InterfazRadio {
 	final double CONST_CAMBIO_FM = 0.2; 
 	final int MAX_AM = 1610;
 	final int MIN_AM = 530;
-	final int MAX_FM = 107.9;
-	final int MIN_FM = 87.9;
+	final double MAX_FM = 107.9;
+	final double MIN_FM = 87.9;
 	
 	
-	public void EncenderApagar(){
-		
-	}
+	public void EncenderApagar();
 	
-	public void CambiarFrec(){
-		
-	}
+	public void CambiarFrec();
 	
-	public void CargarEmisora(double emisora){
-		
-	}
+	public void CargarEmisora(double emisora);
 	
-	public void AdelantarEmisora(){
-		
-	}
+	public void AdelantarEmisora();
 	
-	public void AtrasarEmisora(){
-			
-	}
+	public void AtrasarEmisora();
 	
-	public void GuardarEmisora(double emisora, int posicion){
-		
-		
-	}
+	public void GuardarEmisora(double emisora, int posicion);
 	
-	public void SubirVolumen(){
-		
-	}
+	public void SubirVolumen();
 	
-	public void BajarVolumen(){
-		
-	}
+	public void BajarVolumen();
 	
 }
