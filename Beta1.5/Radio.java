ac39936629de34d759bc06ff38583517546dd35a
octajul77 <gon14096@uvg.edu.gt>
2015-07-11 09:59:05 -0600

    Update Radio.java

--- Beta1.5/Radio.java
+++ Beta1.5/Radio.java
@@ -1,10 +1,14 @@
 public class Radio implements InterfazRadio {
 	private boolean Encendido;
 	private String Frec;
+	private double num;
+	private double num1;
 	public Radio() {
 		// TODO Auto-generated constructor stub
 		Encendido = false;
 		Frec = "AM";
+	    num=minAM();
+		num1=minFM();
 	}
 	public void EncenderApagar()
 	{
@@ -45,6 +49,32 @@ public class Radio implements InterfazRadio {
 	{
 		if(Encendido)
 		{
+		if(Frec == "AM")
+			{
+			num=num+getconstCambioAM();
+			System.out.println(num);
+				if (num>=maxAM())
+				{
+					num=minAM();
+				
+				}else{
+					num=num;
+					}
+			} 
+			else {
+			num1=num1+constCambioFM();
+			System.out.println(num1);
+			if (num1>=maxFM())
+				{
+					num1=minFM();
+				
+				}else{
+					num1=num1;
+					}
+			
+				
+			}
+		
 			System.out.println("Adelantar Emisora");
 		}
 	}
@@ -54,10 +84,34 @@ public class Radio implements InterfazRadio {
 	{
 		if(Encendido)
 		{
-			System.out.println("Atrasar Emisora");
-		}
+		if(Frec == "AM")
+			{
+			num=num-getconstCambioAM();
+			System.out.println(num);
+				if (num<=minAM())
+				{
+					num=minAM();
+				
+				}else{
+					num=num;
+					}
+			} 
+			else {
+			num1=num1-constCambioFM();
+			System.out.println(num1);
+				if (num1<=maxFM())
+				{
+					num1=minFM();
+				
+				}else{
+					num1=num1;
+			}
 		
-	};
+			System.out.println("Atrasar Emisora");
+		        }
+		
+	    };
+	}
 	
 	public void GuardarEmisora(double emisora, int posicion)
 	{
@@ -97,6 +151,27 @@ public class Radio implements InterfazRadio {
 			return "Radio 1.0";
 		}
 	}
+	
+	public String getNum()
+	{
+		if (Encendido)
+		{
+			if(Frec == "AM")
+			{
+				String num2 = Double.toString(num);
+				return num2;
+				
+			} else {
+					String num3 = Double.toString(num1);
+					return num3;
+			
+					}
+		}else {
+			return "Radio 1.0";
+		}
+	};
+	
+	
 	public int getconstCambioAM()
 	{
 		return constCambioAM;
@@ -126,4 +201,5 @@ public class Radio implements InterfazRadio {
 	{
 		return minFM;
 	}
+	
 }
