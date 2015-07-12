3152f150e1ce516654d2667b1011309018f24a0b
Link-djst <Link-djst@users.noreply.github.com>
2015-07-12 08:32:37 -0600

    Update InterfazRadio.java
    Interfaz "Oficial"

--- Beta1.9/InterfazRadio.java
+++ Beta1.9/InterfazRadio.java
@@ -1,24 +1,26 @@
 public interface InterfazRadio {
-	final int constCambioAM = 10;
-	final double constCambioFM = 0.2;
-	final int maxAM = 1610;
-	final int minAM = 530;
-	final double minFM = 87.9;
-	final double maxFM = 107.9;
-
-	public void EncenderApagar();
-
-	public void CambiarFrec();
-
-	public void CargarEmisora(double emisora);
-
-	public void AdelantarEmisora();
-
-	public void AtrasarEmisora();
-
-	public void GuardarEmisora(double emisora, int posicion);
-
-	public void SubirVolumen();
-
-	public void BajarVolumen();
+	final int CONST_CAMBIO_AM = 10;
+	final double CONST_CAMBIO_FM = 0.2; 
+	final int MAX_AM = 1610;
+	final int MIN_AM = 530;
+	final double MAX_FM = 107.9;
+	final double MIN_FM = 87.9;
+	
+	
+	public void encenderApagar();
+	
+	public void cambiarFrec();
+	
+	public void cargarEmisora(double emisora);
+	
+	public void adelantarEmisora();
+	
+	public void atrasarEmisora();
+	
+	public void guardarEmisora(double emisora, int posicion);
+	
+	public void subirVolumen();
+	
+	public void bajarVolumen();
+	
 }
