126b92603ec73796b8f87fdbcf79207db91414c5
mor14012 <mor14012@uvg.edu.gt>
2015-07-11 23:56:31 -0600

    Beta 1.9 (Final Build)
    - Mejoras en Interfaz gráfica y audio para aumentar el realismo.
    - Nuevo archivo Main.java
    - Corrección de error en el que los botones de presets no cambiaban la
    canción.
    - Ordenamiento de código de archivos (solo falta ordenar el GUI)
    - Se cambió el nombre de GUII a GUI.

---
+++ Beta1.9/Radio.java
@@ -0,0 +1,183 @@
+public class Radio implements InterfazRadio {
+	private boolean Encendido;
+	private String Frec;
+	private double AM;
+	private double FM;
+	private double[] FMArray;
+	private double[] AMArray;
+	private double nEmisora;
+	public Radio() {
+		Encendido = false;
+		Frec = "AM";
+	    AM = minAM();
+		FM = minFM();
+		nEmisora = 0.0;
+		FMArray = new double[12];
+		AMArray = new double[12];
+		for(int i=0; i<12; i++){
+			FMArray[i] = 0.0;
+			AMArray[i] = 0.0;
+		}
+	}
+	public void EncenderApagar(){
+		if (!Encendido){
+			Encendido = true;
+		}
+		else{
+			Encendido = false;
+		}
+		System.out.println("Encender Apagar");
+		System.out.println(Encendido);
+	}
+	public void CambiarFrec(){
+		if (Encendido){
+			if(Frec == "AM"){
+				Frec = "FM";
+			}
+			else{
+				Frec = "AM";
+			}
+			System.out.println("Cambiar Frec");
+			System.out.println("La Frecuencia es: "+Frec);
+		}
+	}
+	public void CargarEmisora(double emisora){
+		if (Encendido){
+			if(Frec == "AM"){
+				AM = emisora;
+			}
+			else{
+				FM = emisora;
+			}
+		}
+	}
+	public void AdelantarEmisora(){
+		if(Encendido){
+		if(Frec == "AM"){
+			AM=AM+getconstCambioAM();
+			System.out.println(AM);
+			if (AM>=maxAM()){
+				AM=minAM();
+			}
+		}
+		else{
+			FM=FM+constCambioFM();
+			System.out.println(FM);
+			if (FM>=maxFM()){
+				FM=minFM();
+			}
+		}
+		System.out.println("Adelantar Emisora");
+		}
+	}
+	public void AtrasarEmisora(){
+		if(Encendido){
+		if(Frec == "AM"){
+			AM=AM-getconstCambioAM();
+			System.out.println(AM);
+			if (AM<=minAM()){
+				AM=minAM();
+			}
+		}
+		else {
+			FM=FM-constCambioFM();
+			System.out.println(FM);
+			if (FM<=minFM()){
+				FM=minFM();
+			}
+		}
+		System.out.println("Atrasar Emisora");
+		}
+	}
+	public void GuardarEmisora(double emisora, int posicion){
+		if(Encendido){
+			System.out.println("Guardar Emisora");
+			if (Frec=="AM"){
+				AMArray[posicion-1]=emisora;
+				System.out.println(emisora);
+				System.out.println("Guardado "+AMArray[posicion-1]+" en el array de AM en la posicion "+(posicion-1));
+			}
+			else{
+				FMArray[posicion-1]=emisora;
+				System.out.println(emisora);
+				System.out.println("Guardado "+FMArray[posicion-1]+" en el array de FM en la posicion "+(posicion-1));
+			}
+		}
+	}
+	public double cEmisora(int posicion){
+		if(Encendido){
+			System.out.println("Cargando Emisora");
+			if (Frec=="AM"){
+				nEmisora = AMArray[posicion-1];
+			}
+			else{
+				nEmisora = FMArray[posicion-1];
+			}
+		}
+		return nEmisora;
+	}
+	public void SubirVolumen(){
+		if(Encendido){
+			System.out.println("Subir Volumen");
+		}
+	}
+	public void BajarVolumen(){
+		if(Encendido){
+			System.out.println("Bajar Volumen");
+		}
+	}
+	public boolean getEncendido(){
+		return Encendido;
+	}
+	public String getFrec(){
+		if (Encendido){
+			return Frec;
+		}
+		else{
+			return "       Radio 2DJ";
+		}
+	}
+	public void changeFrec(){
+		if (Frec=="AM"){
+			Frec = "FM";
+		}
+		else{
+			Frec = "AM";
+		}
+	}
+	public String getNum(){
+		if (Encendido)
+		{
+			if(Frec == "AM"){
+				String num2 = Double.toString(AM);
+				return num2;
+			}
+			else{
+				String num3 = Double.toString(FM);
+				num3+="00";
+				return num3.substring(0,5);
+			}
+		}
+		else{
+			return "       Radio 2DJ";
+		}
+	}
+	public int getconstCambioAM(){
+		return constCambioAM;
+	}
+	public double constCambioFM(){
+		return constCambioFM;
+	}
+	public int maxAM(){
+		return maxAM;
+	}
+	public int minAM(){
+		return minAM;
+	}
+	public double maxFM(){
+		return maxFM;
+	}
+	public double minFM(){
+		return minFM;
+	}
+}
