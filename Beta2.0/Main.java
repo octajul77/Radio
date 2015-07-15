295f60a146312da3e66b2ad7a80309351fc9c47c
octajul77 <gon14096@uvg.edu.gt>
2015-07-14 20:31:36 -0600

    Update Main.java

--- Beta2.0/Main.java
+++ Beta2.0/Main.java
@@ -1,8 +1,16 @@
-import java.awt.*;
+import java.awt.EventQueue;
 
+/**
+ * @author Julio Gonzalez, Diego Morales, Diego Sosa
+ * @version 2.0
+ */
 class Main{
 	public static void main (String [] args){
 		EventQueue.invokeLater(new Runnable(){
+			/* (non-Javadoc)
+			 * @see java.lang.Runnable#run()
+			 * Permite correr el programa y mostrar la interfaz
+			 */
 			public void run(){
 				try{
 					GUI frame = new GUI();
